da238062052aa94c4b951276b11a617ee84e7632
jasonsummers012 <jasonsummers012@gmail.com>
2025-05-29 21:49:55 -0600

    Load in and begin filtering RNA-seq data

--- scripts/01_data_loading_and_qc.R
+++ scripts/01_data_loading_and_qc.R
@@ -1,3 +1,25 @@
 library(Seurat)
 library(Matrix)
 library(tidyverse)
+library(ggplot2)
+
+data = Read10X_h5(filename = "data/5k_human_melanoma_tumor_CNIK_5pv2_filtered_feature_bc_matrix.h5")
+counts = data
+
+melanoma_seurat = CreateSeuratObject(
+  counts = counts,
+  project = "melanoma",
+  min.cells = 3,
+  min.features = 200
+)
+
+melanoma_seurat[["percent.mt"]] = PercentageFeatureSet(
+  melanoma_seurat,
+  pattern = "^MT-"
+)
+
+VlnPlot(
+  melanoma_seurat,
+  features = c("nFeature_RNA", "nCount_RNA", "percent.mt"),
+  ncol = 3
+)
